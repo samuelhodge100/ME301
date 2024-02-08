6db8c768ce4e82f76c210b0f06318af7037b0bcd
Samuel Hodge <samuelhodge100@gmail.com>
2024-02-08 12:34:33 -0600

    Some updates

--- ASSN 2/testing.py
+++ ASSN 2/testing.py
@@ -2,6 +2,7 @@ import map
 from map import DIRECTION
 
 DIR = {'North':1,'East':2,'South':3,'West':4}
+DIR_INV = {1:'North', 2:'East', 3:'South', 4:'West'}
 
 def waveform_planning(map,start_position,end_position):
     # Start (i,j) in map coordiates
@@ -19,13 +20,13 @@ def waveform_planning(map,start_position,end_position):
 
 def map_move(position,dir):
     # Inputs a position and a direction and outputs the updated position
-    if dir==1:
+    if dir==1: # North
         position = [position[0]-1, position[1]]
-    elif dir==2:
+    elif dir==2: # East
         position = [position[0],position[1]+1]
-    elif dir==3:
+    elif dir==3: # South
         position = [position[0]+1,position[1]]
-    elif dir==4:
+    elif dir==4: # West
         position = [position[1], position[1]-1]
     return position
 
@@ -38,17 +39,18 @@ def waveform_check_neighbors(map, current_positions,val):
     for current_position in current_positions:
         for dir in range(1,5):
             if map.getNeighborObstacle(current_position[0],current_position[1],dir):
-                print(f'There is an obsticle to the {dir} of position {current_position}')
+                print(f'There is an obsticle to the {DIR_INV[dir]} of position {current_position}')
             elif(map.getNeighborCost(current_position[0],current_position[1],dir) != 0):
-                print(f'The cell {map_move(current_position,dir)} is already occupied')
+                print(f'The cell {map_move(current_position,dir)} is already occupied with a cost!')
             else:
                 # Assign a cost to that cell and add it to the positions list
                 map.setNeighborCost(current_position[0],current_position[1],dir,val)
                 new_positions.append(map_move(current_position,dir))
+                
                 map.printObstacleMap()
                 map.printCostMap()
                 print('\n')
-    
+    print(f'Here are the positions being tracked : {new_positions}')
     val+=1
     waveform_check_neighbors(map,new_positions,val)
                 
